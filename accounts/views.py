6da4e2d982db253b76a081ce5ddf004fa2d87a1f
sanzid imran <sanzidimran@gmail.com>
2019-09-29 09:32:45 +0600

    Submitting data for a user through the registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
-
+from django.contrib.auth.models import User, auth
 
 def register(request):
-    return render(request, 'register.html')
+
+    if request.method =='POST':
+        username=request.POST['username'] 
+        email=request.POST['email'] 
+        password1=request.POST['password1'] 
+        password2=request.POST['password2'] 
+
+        user=User.objects.create_user(username=username, email=email, password=password1)
+        user.save()
+        return redirect('/')
+    
+    
+    else:
+        return render(request, 'register.html')
