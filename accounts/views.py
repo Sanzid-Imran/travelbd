870f7411c75db7063a0035223211016dfece8a03
sanzid imran <sanzidimran@gmail.com>
2019-09-29 23:21:36 +0600

    checking the password's been matching or username or email is taken or not and showing messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-# Create your views here.
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+
+
+# Create your views here.
 
 def register(request):
 
@@ -10,10 +14,23 @@ def register(request):
         password1=request.POST['password1'] 
         password2=request.POST['password2'] 
 
-        user=User.objects.create_user(username=username, email=email, password=password1)
-        user.save()
+        if password1== password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken !')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken !')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                print('User Created !')
+                return redirect('/')
+        else:
+            messages.info(request, 'Password not matched ')
+            return redirect('register')
         return redirect('/')
-    
-    
+
+
     else:
         return render(request, 'register.html')
